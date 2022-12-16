7385e8453cefbd6a95b1b14b7fe446d41e77ef3d
Sushant Gupta <sushantg2001@gmail.com>
2022-12-16 10:57:50 +0530

    prod settings changed

--- drive_backend/config/settings/local.py
+++ drive_backend/config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="UcFf9VLqje79OfwL6CEpU0MZFwnQ0TVbdYz3GdygFhxNBUKDrpsynEGaql70crEH",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- drive_backend/config/settings/production.py
+++ drive_backend/config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["0.0.0.0"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -92,21 +92,11 @@ EMAIL_SUBJECT_PREFIX = env(
 
 # ADMIN
 # ------------------------------------------------------------------------------
-# Django Admin URL regex.
-ADMIN_URL = env("DJANGO_ADMIN_URL")
-
 # Anymail
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ["anymail"]  # noqa F405
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/mailjet/
-EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
-ANYMAIL = {
-    "MAILJET_API_KEY": env("MAILJET_API_KEY"),
-    "MAILJET_SECRET_KEY": env("MAILJET_SECRET_KEY"),
-}
 
 # Collectfast
 # ------------------------------------------------------------------------------
